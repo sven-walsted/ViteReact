48d3428ef469efcc3dda23259a730e95aa96b2b2
Sven Walsted <swalsted@gmail.com>
2026-02-14 14:52:15 -0900

    Update clean_and_copy_build.sh
    Remove commented code

--- jakartaee/clean_and_copy_build.sh
+++ jakartaee/clean_and_copy_build.sh
@@ -1,11 +1,6 @@
 #!/usr/bin/bash
 
 SCRIPT_DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" >/dev/null 2>&1 && pwd )"
-# echo "The script's absolute directory is: ${SCRIPT_DIR}"
-
-# To get the full path including the script name:
-# SCRIPT_PATH="$SCRIPT_DIR/$(basename "$BASH_SOURCE")"
-# echo "The script's absolute path is: $SCRIPT_PATH"
 
 rm -Rf ${SCRIPT_DIR}/src
 mkdir -p ${SCRIPT_DIR}/src/main/resources/META-INF/resources
