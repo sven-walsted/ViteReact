f4e795fabe7fb9ca40cfde8b687b602fe5018d73
Sven Walsted <swalsted@gmail.com>
2026-02-14 14:56:30 -0900

    BUGFIX: remove build/ from resources/

--- jakartaee/clean_and_copy_build.sh
+++ jakartaee/clean_and_copy_build.sh
@@ -5,4 +5,4 @@ SCRIPT_DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" >/dev/null 2>&1 && pwd )"
 rm -Rf ${SCRIPT_DIR}/src
 mkdir -p ${SCRIPT_DIR}/src/main/resources/META-INF/resources
 
-cp -Rf ${SCRIPT_DIR}/../react/dist ${SCRIPT_DIR}/src/main/resources/META-INF/resources
+cp -Rf ${SCRIPT_DIR}/../react/dist/* ${SCRIPT_DIR}/src/main/resources/META-INF/resources
