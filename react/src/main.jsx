00e683590c71c96ad3b3a723084ad4e0cb71cc5e
Sven Walsted <1226167915113302@mil>
2026-03-06 14:34:18 -0900

    Integrate with jakaraee-essentials

--- react/src/main.jsx
+++ react/src/main.jsx
@@ -5,7 +5,7 @@ import App from './App.jsx'
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
-    <BrowserRouter>
+    <BrowserRouter  basename="/jakartaee-essentials">
       <App />
     </BrowserRouter>
   </React.StrictMode>
